aa4a17d132d4f9a81cfb1b01b63c4a37937e989e
isabella chen <isabella.chen@mail.utoronto.ca>
2024-05-13 16:54:24 -0700

    add new testing file

---
+++ server/test/data_strategy.ts
@@ -0,0 +1,23 @@
+import sinon from 'sinon';
+import { expect } from 'chai';
+import { TextLoader } from 'langchain/document_loaders/fs/text';
+import { Document } from 'langchain/document';
+import { FileDataObtainingStrategy } from '../routes/data_strategy';
+import { MemoryVectorStore } from 'langchain/vectorstores/memory';
+
+describe('FileDataObtainingStrategy', () => {
+    describe("#obtainVectorStore", () => {
+        it('should obtain vector store from txt file', async () => {
+            const filePath = 'test.txt';
+            const expectedDocs = [new Document({ pageContent: 'Test content' })]
+            
+            // mocks new Textloader.load
+            const loaderStub = sinon.stub(TextLoader.prototype, 'load').resolves(expectedDocs)
+
+            const strategy = new FileDataObtainingStrategy();
+            const vectorStore = await strategy.obtainVectorStore(filePath)
+
+            expect(vectorStore).to.be.instanceOf(MemoryVectorStore);
+        })
+    })
+})
